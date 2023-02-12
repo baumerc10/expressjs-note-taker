9e71dbc98f34e61eb2bee512ec58e404655b28b4
Cody Kirschbaum <baumerc10@gmail.com>
2023-02-11 20:34:55 -0500

    functions for get/add notes, configured apiRoutes

---
+++ routes/apiRoutes.js
@@ -0,0 +1,23 @@
+const router = require('express').Router();
+const dataStore = require(../db/dataStore);
+
+// GET
+router.get('/notes', function(req, res) {
+    dataStore
+        .getNotes()
+        .then(notes => res.json(notes))
+        .catch(err => res.status(500).json(err));
+});
+
+// POST
+router.post('/notes', function(req, res) {
+    dataStore  
+        .addNote(req.body)
+        .then((notes) => res.json(notes))
+        .catch(err => res.status(500).json(err));
+});
+
+// Bonus
+
+
+module.exports = router;
